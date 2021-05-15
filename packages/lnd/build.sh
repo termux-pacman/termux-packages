d07ee7036003219ebdc0b263d68a3dc34d1dff54
Henrik Grimler <henrik@grimler.se>
2021-05-15 20:53:59 +0200

    lnd: use TERMUX_PKG_SERVICE_SCRIPT for service script

--- packages/lnd/build.sh
+++ packages/lnd/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_SRCURL=(https://github.com/lightningnetwork/lnd/archive/v${TERMUX_PKG
 TERMUX_PKG_SHA256=(c0caca7c045e92b9dfafe98a9facfb9741dba3bb4bc0da6f4629e5abd9bf0aff
                    03a5a367b62ffa4f4ffa3de4a00f5e79b94449297ec6760223e2f8193c286529)
 TERMUX_PKG_DEPENDS="bitcoin"
-TERMUX_PKG_CONFFILES="var/service/lnd/run var/service/lnd/log/run"
+TERMUX_PKG_SERVICE_SCRIPT=("lnd" 'exec bitcoind 2>&1')
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
@@ -19,16 +19,4 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 lnd lncli "$TERMUX_PREFIX"/bin/
-}
-
-termux_step_post_make_install() {
-	mkdir -p $TERMUX_PREFIX/var/service
-	cd $TERMUX_PREFIX/var/service
-	mkdir -p lnd/log
-	echo "#!$TERMUX_PREFIX/bin/sh" > lnd/run
-	echo 'exec lnd 2>&1' >> lnd/run
-	chmod +x lnd/run
-	touch lnd/down
-
-	ln -sf $TERMUX_PREFIX/share/termux-services/svlogger lnd/log/run
 }
