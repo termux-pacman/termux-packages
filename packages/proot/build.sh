53eb8acdb4ffdcd61b822d82e1eaa5467dcef1a0
Henrik Grimler <henrik@grimler.se>
2021-08-10 14:03:51 +0200

    proot: do not override termux_step_make{,_install}

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -9,6 +9,8 @@ TERMUX_PKG_REVISION=43
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=99d6fc4a1217ce304292c58c02042d4b30d322f86f8f9466d7a50b5fca3c391b
 TERMUX_PKG_DEPENDS="libtalloc"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="-C src"
 
 # Install loader in libexec instead of extracting it every time
 export PROOT_UNBUNDLE_LOADER=$TERMUX_PREFIX/libexec/proot
@@ -17,11 +19,7 @@ termux_step_pre_configure() {
 	CPPFLAGS+=" -DARG_MAX=131072"
 }
 
-termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR/src
-	make V=1
-	make install
-
+termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp $TERMUX_PKG_SRCDIR/doc/proot/man.1 $TERMUX_PREFIX/share/man/man1/proot.1
 
