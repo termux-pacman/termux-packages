ff691e82aa1dd537317d8a992b15763fecc4dcf2
Henrik Grimler <henrik@grimler.se>
2021-09-08 15:01:05 +0200

    helix: install to TERMUX_PKG_MASSAGEDIR

--- packages/helix/build.sh
+++ packages/helix/build.sh
@@ -8,27 +8,31 @@ TERMUX_PKG_SRCURL="https://github.com/helix-editor/helix.git"
 TERMUX_PKG_GIT_BRANCH="v$TERMUX_PKG_VERSION"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make_install() {
+termux_step_make() {
 	termux_setup_rust
 
 	sed -i "s%\@TERMUX_CC\@%${CC}%g" ./helix-syntax/build.rs
 
 	cargo build --jobs "${TERMUX_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
+}
 
+termux_step_make_install() {
 	cat > "hx" <<- EOF
 	#!${TERMUX_PREFIX}/bin/sh
 
 	HELIX_RUNTIME=${TERMUX_PREFIX}/lib/helix/runtime \\
 	exec ${TERMUX_PREFIX}/lib/helix/hx "\$@"
 	EOF
-	install -Dm755 ./hx "${TERMUX_PREFIX}/bin/hx"
+	install -Dm755 ./hx "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin/hx"
 
-	mkdir -p "${TERMUX_PREFIX}/lib/helix"
+	mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib/helix"
 
 	cp -r runtime "${TERMUX_PREFIX}/lib/helix"
 
-	install -Dm755 -t "${TERMUX_PREFIX}/lib/helix" "target/${CARGO_TARGET_NAME}/release/hx"
-	install -Dm666 -t "${TERMUX_PREFIX}/lib/helix" languages.toml
+	install -Dm755 -t "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib/helix" \
+		"target/${CARGO_TARGET_NAME}/release/hx"
+	install -Dm666 -t "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib/helix" \
+		languages.toml
 }
 
 termux_step_create_debscripts() {
