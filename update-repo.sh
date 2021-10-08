4bb4674a05d156b09419aefe4d6bc14b0cb62461
Maxython <mixython@gmail.com>
2021-10-08 16:56:03 +0300

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+info() {
+	echo -e "\033[1;36m\n==> $1\033[0m"
+}
+
+commet() {
+	echo -e "\033[0;32m-> $1\033[0m"
+}
+
 # Set repo for auto push
 read -p "Username: " username
 read -sp "Key: " key
@@ -7,26 +15,36 @@ echo
 git remote set-url origin "https://${username}:${key}@github.com/Maxython/termux-packages.git"
 
 # Update repo
-echo "==> Update repo."
+info "Update repo."
 git pull upstream master
+commet "Continue?"
+git status
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) break;;
+		No ) exit 1;;
+	esac
+done
 git reset
 
 # Sort file
-echo "==> Sort and push packages."
+info "Sort and push packages."
 for i in $(git status | grep packages/ | sed "s|modified:||g"); do
 	file_sp=(${i//// })
 	name=${file_sp[1]}
-	echo "-> Package ${name}: ${i}."
+	commet "Package ${name}: ${i}."
 	if [[ -n $old_name ]] && [[ $old_name != $name ]]; then
-		echo "-> Push package ${old_name}."
+		commet "Push package ${old_name}."
 		git commit -m "Update package ${old_name}"
 		git push origin master
 	fi
 	git add $i
 	old_name="$name"
 done
-echo "-> Push package ${old_name}."
+commet "Push package ${old_name}."
 git commit -m "Update package ${old_name}"
 git push origin master
 
-echo "==> Done."
+git remote set-url origin "https://github.com/Maxython/termux-packages"
+
+info "Done."
