77b1a3530eb39b81340a0a972ef0072f74b43af8
Maxython <mixython@gmail.com>
2021-11-02 15:36:22 +0300

    Update package mu

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -2,14 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-<<<<<<< HEAD
-TERMUX_PKG_VERSION=1.6.7
-TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=105df16df5fd6f54d5334cf68d8108f8b0593e37020b2fa8487c2e251ce9d9a2
-=======
-TERMUX_PKG_VERSION=1.6.8
-TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=d175272bec399c956393c8e2afa5e16f503b65417edc7e3224b50edd6d1f9750
->>>>>>> c1f3a1e5aeed62b22d3161c5a6a060a76e29402a
+TERMUX_PKG_VERSION=1.6.9
+TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION-signed/mu-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=8d99430a4f6942eb3122008bb897c4480cda64b30ff4575c32ded86d7edbaf58
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, libc++, libxapian, libgmime"
