036adbefe59c54fb69548cbbeb89e7d4c282d18f
Maxython <mixython@gmail.com>
2021-12-03 00:22:44 +0300

    Update package k9s

--- packages/k9s/build.sh
+++ packages/k9s/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://k9scli.io
 TERMUX_PKG_DESCRIPTION="Kubernetes CLI To Manage Your Clusters In Style!"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.24.15
+TERMUX_PKG_VERSION=0.25.7
 TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=910ce32c3b2f8cf70fa41982e1d2a130e81fd826d43e4a4cee3a74e2f4c15541
+TERMUX_PKG_SHA256=af24547f4c83492ec92db36e3cc09bdbb27aef44969bb18f393e93eee8c5d3a3
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
