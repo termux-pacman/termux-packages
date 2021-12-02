028917e3ef747f26e4f7eab357fa955732606cec
Maxython <mixython@gmail.com>
2021-12-03 00:22:34 +0300

    Update package imagemagick

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,21 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.1.0.13
+TERMUX_PKG_VERSION=7.1.0.16
 TERMUX_PKG_SRCURL=https://download.imagemagick.org/ImageMagick/download/ImageMagick-$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.lz
-TERMUX_PKG_SHA256=22da11ef55c42a7292379bf88f4713c6c931b1302d2c4be83a75eb0186ad458d
+TERMUX_PKG_SHA256=06a9e078a83ab0e226fc69bff1cc2c3e9714e46c500f1f9421dd16ca33f26ebd
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libheif, libjpeg-turbo, libjxl, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib, zstd"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-x
 --without-gvc
---with-magick-plus-plus=no
+--with-magick-plus-plus=yes
 --with-bzlib=yes
 --with-xml=yes
 --with-rsvg=yes
 --with-lzma
 --with-jxl=yes
+--with-fftw
 --disable-openmp
 ac_cv_func_ftime=no
 "
