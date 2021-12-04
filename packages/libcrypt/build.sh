3a0584884228665e9abfe47640046fb27a705254
Maxython <mixython@gmail.com>
2021-12-04 23:39:47 +0300

    packages/libcrypt

--- packages/libcrypt/build.sh
+++ packages/libcrypt/build.sh
@@ -9,6 +9,7 @@ TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BREAKS="libcrypt-dev"
 TERMUX_PKG_REPLACES="libcrypt-dev"
 
+
 termux_step_make_install() {
 	$CC $CFLAGS $CPPFLAGS $LDFLAGS -Wall -Wextra -fPIC -shared $TERMUX_PKG_BUILDER_DIR/crypt3.c -lcrypto -o $TERMUX_PREFIX/lib/libcrypt.so
 	mkdir -p $TERMUX_PREFIX/include/
