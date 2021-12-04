a71070b545d0c0ed6efd69fd1ea3f87095bbac41
Maxython <mixython@gmail.com>
2021-12-05 01:30:45 +0300

    packages/texinfo

--- packages/texinfo/build.sh
+++ packages/texinfo/build.sh
@@ -9,3 +9,4 @@ TERMUX_PKG_SHA256=8eb753ed28bca21f8f56c1a180362aed789229bd62fff58bf8368e9beb59fe
 TERMUX_PKG_DEPENDS="libiconv, ncurses, perl, gawk"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-perl-xs"
 TERMUX_PKG_GROUPS="base-devel"
+
