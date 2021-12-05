26910eab0ee44d7b7d128a2683cdb12090ee7314
Maxython <mixython@gmail.com>
2021-12-05 13:34:22 +0300

    packages/libnpth

--- packages/libnpth/build.sh
+++ packages/libnpth/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/npth/npth-${TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1
 TERMUX_PKG_BREAKS="libnpth-dev"
 TERMUX_PKG_REPLACES="libnpth-dev"
+
