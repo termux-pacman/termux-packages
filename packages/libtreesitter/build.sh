4a497c757ed4957752985af7cd0341469ba250a0
Maxython <mixython@gmail.com>
2021-12-05 15:27:05 +0300

    packages/libtreesitter

--- packages/libtreesitter/build.sh
+++ packages/libtreesitter/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v
 TERMUX_PKG_SHA256=12a3f7206af3028dbe8a0de50d8ebd6d7010bf762db918acae76fc7585f1258d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
