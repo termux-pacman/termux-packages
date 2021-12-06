eb132e1a0e263e568b3db20c39f6e0e4f7f4fd91
Maxython <mixython@gmail.com>
2021-12-06 09:23:24 +0300

    packages/command-not-found

--- packages/command-not-found/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
-TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
-TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.69
-TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=733c4e29b08588691b436373501040512c087dce99982fc00c4fb3fabefe5503
