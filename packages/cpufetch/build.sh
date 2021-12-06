a9baeb8a7aea6a158b104931935d53b67f35df14
Maxython <mixython@gmail.com>
2021-12-06 12:07:39 +0300

    packages/cpufetch

--- packages/cpufetch/build.sh
+++ packages/cpufetch/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_VERSION=1.00
 TERMUX_PKG_SRCURL=https://github.com/Dr-Noob/cpufetch/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2254c2578435cc35c4d325b25fdff4c4b681de92cbce9a7a36e58ad58a3d9173
 TERMUX_PKG_BUILD_IN_SRC=true
+
