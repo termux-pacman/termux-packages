74d35b792bc21af498a041ea95ed95ae0db11d8e
Maxython <mixython@gmail.com>
2021-12-10 23:06:00 +0300

    packages/libmediainfo

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -20,3 +20,4 @@ termux_step_post_make_install() {
 	ln -sf "${TERMUX_PREFIX}/lib/libmediainfo.so" "${TERMUX_PREFIX}/lib/libmediainfo.so.0"
 	ln -sf "${TERMUX_PREFIX}/lib/libmediainfo.so" "${TERMUX_PREFIX}/lib/libmediainfo.so.0.0"
 }
+
