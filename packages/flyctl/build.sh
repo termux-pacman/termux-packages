5b3d5c3522aed7131c020573db304dc8e3e1bc0e
Maxython <mixython@gmail.com>
2021-12-10 14:45:53 +0300

    packages/flyctl

--- packages/flyctl/build.sh
+++ packages/flyctl/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://fly.io
 TERMUX_PKG_DESCRIPTION="Command line tools for fly.io services"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
-TERMUX_PKG_VERSION=0.0.261
+TERMUX_PKG_VERSION=0.0.267
 TERMUX_PKG_SRCURL=https://github.com/superfly/flyctl/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=05eb4f80d98595a8faaaad657e6c695c6955e075dddcb539dad78a10d7036c96
+TERMUX_PKG_SHA256=2886dc647aeea4dd82c80892cc1ec436a670a36691f6c54688478513df979a08
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, arm"
+
 
 termux_step_post_get_source() {
 	termux_setup_golang
