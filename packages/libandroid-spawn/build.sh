97e3787dc97b08e9894968fa96687e7e012d3ac9
Maxython <mixython@gmail.com>
2021-12-11 11:09:22 +0300

    packages/libandroid-spawn

--- packages/libandroid-spawn/build.sh
+++ packages/libandroid-spawn/build.sh
@@ -18,4 +18,3 @@ termux_step_make_install() {
 	install -Dm600 libandroid-spawn.a $TERMUX_PREFIX/lib/libandroid-spawn.a
 	install -Dm600 libandroid-spawn.so $TERMUX_PREFIX/lib/libandroid-spawn.so
 }
-
