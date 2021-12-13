e6aef4fd6d588f66e3b974e703643c2cab4f2d99
Maxython <mixython@gmail.com>
2021-12-13 13:16:04 +0300

    Test

--- packages/pulseaudio/build.sh
+++ packages/pulseaudio/build.sh
@@ -37,11 +37,7 @@ termux_step_pre_configure() {
 		_NDK_ARCHNAME=x86
 	fi
 	mkdir $TERMUX_PKG_TMPDIR/libaaudio
-	echo "----------"
-	ls $NDK/*
-	echo "----------"
-	ls $NDK/*/*
-	echo "----------"
+	ls $NDK/prebuilt/android-arm/gdbserver
 	cp $NDK/platforms/android-26/arch-$_NDK_ARCHNAME/usr/lib*/libaaudio.so \
 		$TERMUX_PKG_TMPDIR/libaaudio/
 	LDFLAGS+=" -L$TERMUX_PKG_TMPDIR/libaaudio/"
