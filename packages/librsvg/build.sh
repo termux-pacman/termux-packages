f1dcd18b0074d904465365c0e5413fb9005cfe82
Maxython <mixython@gmail.com>
2021-12-14 14:24:58 +0300

    packages/librsvg

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.52.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=949dffcb0414865409e17a6c89ae30bc8bb014a51fba5186f73d1a46c6c5ccde
 TERMUX_PKG_DEPENDS="gdk-pixbuf, libcairo, libcroco, pango, zlib"
@@ -12,12 +12,18 @@ TERMUX_PKG_REPLACES="librsvg-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_GDK_PIXBUF_QUERYLOADERS=$TERMUX_PREFIX/bin/gdk-pixbuf-query-loaders
 --disable-introspection
+--disable-static
 "
 
 termux_step_pre_configure() {
 	termux_setup_rust
 
 	LDFLAGS+=" -fuse-ld=lld"
+
+	# Work around https://gitlab.gnome.org/GNOME/librsvg/-/issues/820
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		LDFLAGS+=" -Wl,-z,muldefs"
+	fi
 
 	# See https://github.com/GNOME/librsvg/blob/master/COMPILING.md
 	export RUST_TARGET=$CARGO_TARGET_NAME
