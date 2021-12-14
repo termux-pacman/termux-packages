8a97dd5591b0b0e4df2b6ea4e865ee38659c81f0
Maxython <mixython@gmail.com>
2021-12-14 14:01:09 +0300

    packages/libgit2

--- packages/libgit2/build.sh
+++ packages/libgit2/build.sh
@@ -10,3 +10,4 @@ TERMUX_PKG_DEPENDS="libcurl, openssl, libssh2, zlib, libiconv"
 TERMUX_PKG_BREAKS="libgit2-dev"
 TERMUX_PKG_REPLACES="libgit2-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_CLAR=OFF"
+
