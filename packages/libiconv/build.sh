ed73db91743b9ca465978daac87025669cbec7ac
Maxython <mixython@gmail.com>
2021-12-27 01:31:18 +0300

    packages/libiconv

--- packages/libiconv/build.sh
+++ packages/libiconv/build.sh
@@ -9,6 +9,5 @@ TERMUX_PKG_SHA256=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc
 TERMUX_PKG_BREAKS="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 TERMUX_PKG_REPLACES="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 
-
 # Enable extra encodings (such as CP437) needed by some programs:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-extra-encodings"
