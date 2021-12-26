7e90f4b8d79140b56aee29f3e96fe1d20f839583
Maxython <mixython@gmail.com>
2021-12-27 01:30:39 +0300

    packages/libandroid-wordexp

--- packages/libandroid-wordexp/build.sh
+++ packages/libandroid-wordexp/build.sh
@@ -17,4 +17,3 @@ termux_step_make_install() {
 	install -Dm600 libandroid-wordexp.a $TERMUX_PREFIX/lib/libandroid-wordexp.a
 	install -Dm600 libandroid-wordexp.so $TERMUX_PREFIX/lib/libandroid-wordexp.so
 }
-
