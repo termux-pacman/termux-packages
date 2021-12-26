cc0837f044a8761dcb75ce2f466d9b220d605bdd
Maxython <mixython@gmail.com>
2021-12-27 01:35:13 +0300

    packages/zlib

--- packages/zlib/build.sh
+++ packages/zlib/build.sh
@@ -9,7 +9,6 @@ TERMUX_PKG_SHA256=4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf0
 TERMUX_PKG_BREAKS="ndk-sysroot (<< 19b-3), zlib-dev"
 TERMUX_PKG_REPLACES="ndk-sysroot (<< 19b-3), zlib-dev"
 
-
 termux_step_configure() {
 	"$TERMUX_PKG_SRCDIR/configure" --prefix=$TERMUX_PREFIX
 	sed -n '/Copyright (C) 1995-/,/madler@alumni.caltech.edu/p' "$TERMUX_PKG_SRCDIR/zlib.h" > "$TERMUX_PKG_SRCDIR/LICENSE"
