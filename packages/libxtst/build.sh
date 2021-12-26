cfae7134f6bed42e36decb681721e1c822b889ed
Maxython <mixython@gmail.com>
2021-12-27 01:33:22 +0300

    packages/libxtst

--- packages/libxtst/build.sh
+++ packages/libxtst/build.sh
@@ -9,4 +9,3 @@ TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXtst-$
 TERMUX_PKG_SHA256=4655498a1b8e844e3d6f21f3b2c4e2b571effb5fd83199d428a6ba7ea4bf5204
 TERMUX_PKG_DEPENDS="libx11, libxext, libxi"
 TERMUX_PKG_BUILD_DEPENDS="libxfixes, xorg-util-macros"
-
