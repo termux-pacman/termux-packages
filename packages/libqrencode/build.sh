e820973c6fa0c388a71dab79f767a996a3b1aa3c
Maxython <mixython@gmail.com>
2021-12-27 01:32:09 +0300

    packages/libqrencode

--- packages/libqrencode/build.sh
+++ packages/libqrencode/build.sh
@@ -10,4 +10,3 @@ TERMUX_PKG_DEPENDS="libandroid-support, libpng, zlib"
 TERMUX_PKG_BREAKS="libqrencode-dev"
 TERMUX_PKG_REPLACES="libqrencode-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
-
