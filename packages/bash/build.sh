e261d1050116e55800f547f9714c73cb291e6930
Maxython <mixython@gmail.com>
2021-12-27 01:29:19 +0300

    packages/bash

--- packages/bash/build.sh
+++ packages/bash/build.sh
@@ -15,7 +15,6 @@ TERMUX_PKG_REPLACES="bash-dev"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-multibyte --without-bash-malloc --with-installed-readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_job_control_missing=present"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_sys_siglist=yes"
