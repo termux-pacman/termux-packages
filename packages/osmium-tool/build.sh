683093c2f2dab10a186e4aff3cd990723f504653
Ivan Max <60917834+Maxython@users.noreply.github.com>
2022-03-05 12:45:17 +0300

    osmium-tool

--- packages/osmium-tool/build.sh
+++ packages/osmium-tool/build.sh
@@ -10,3 +10,4 @@ TERMUX_PKG_SHA256=d6273e2614d390d8444b767018b7023bdac3538cbe094d2799eee50b6f08cd
 TERMUX_PKG_DEPENDS="libosmium, libprotozero, boost, libbz2, zlib, liblz4, libexpat"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 TERMUX_PKG_GROUPS="science"
+
