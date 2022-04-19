1496c841ecf9b62daa28c8573161e7787ec18b04
Ivan Max <60917834+Maxython@users.noreply.github.com>
2022-04-20 00:02:17 +0300

    iw - test

--- root-packages/iw/build.sh
+++ root-packages/iw/build.sh
@@ -7,4 +7,3 @@ TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/software/network/iw/iw-${T
 TERMUX_PKG_SHA256=4c44e42762f903f9094ba5a598998c800a97a62afd6fd31ec1e0a799e308659c
 TERMUX_PKG_DEPENDS="libnl"
 TERMUX_PKG_BUILD_IN_SRC=true
-
