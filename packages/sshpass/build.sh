60a1115067fcb8c0152e0cf4fb854e8fde085fb1
Ivan Max <mixython@gmail.com>
2022-10-14 15:23:06 +0300

    build sshpass (#2)

--- packages/sshpass/build.sh
+++ packages/sshpass/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.09
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/sshpass/sshpass/${TERMUX_PKG_VERSION}/sshpass-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7
+
