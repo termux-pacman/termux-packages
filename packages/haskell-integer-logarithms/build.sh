905b5e59a0d820e63698ff4c0b42752bafc5b602
Ivan Max <mixython@gmail.com>
2022-10-15 14:44:56 +0300

    build haskell-integer-logarithms (#2)

--- packages/haskell-integer-logarithms/build.sh
+++ packages/haskell-integer-logarithms/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/integer-logarithms-${TERM
 TERMUX_PKG_SHA256="9b0a9f9fab609b15cd015865721fb05f744a1bc77ae92fd133872de528bbea7f"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="ghc-libs"
+
