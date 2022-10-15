4ca5990945a43e6b8772d4b21b676960c0cbe136
Ivan Max <mixython@gmail.com>
2022-10-15 17:09:22 +0300

    build haskell-witherable (#2)

--- packages/haskell-witherable/build.sh
+++ packages/haskell-witherable/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/witherable-${TERMUX_PKG_V
 TERMUX_PKG_SHA256="790d2bb274283419173bd89104439860675a9410f70f21912973ecd9098b4104"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="ghc-libs, haskell-vector, haskell-hashable, haskell-base-orphans, haskell-indexed-traversable-instances, haskell-indexed-traversable, haskell-unordered-containers"
+
