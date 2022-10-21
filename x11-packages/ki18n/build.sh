fbfd51d1da92e9f6a45048a4fda45862394040b7
Ivan Max <mixython@gmail.com>
2022-10-21 13:22:37 +0300

    build ki18n (#1)

--- x11-packages/ki18n/build.sh
+++ x11-packages/ki18n/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=f2fc8c40c10576da8b74070b7dc8e752fdd04204cb2bfe522f37a0458fbaf881
 TERMUX_PKG_DEPENDS="qt5-qtbase, gettext"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, python, qt5-qtbase-cross-tools, qt5-qtdeclarative"
+
