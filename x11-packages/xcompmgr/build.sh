497682486f207ffa52bbf82edf50998c77c8c6a6
Ivan Max <mixython@gmail.com>
2022-10-21 13:47:57 +0300

    build xcompmgr (#1)

--- x11-packages/xcompmgr/build.sh
+++ x11-packages/xcompmgr/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_REVISION=22
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xcompmgr-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=0b89daf4f060fa3fa83cca862c45d531fef7121b5d06552c1d73d396ad9a7243
 TERMUX_PKG_DEPENDS="libx11, libxcomposite, libxdamage, libxext, libxfixes, libxrender"
+
