75373cac2e3d72ec13a04d3e1ca1af4b6afc15e2
Ivan Max <mixython@gmail.com>
2022-10-21 13:42:35 +0300

    build sdl-image (#1)

--- x11-packages/sdl-image/build.sh
+++ x11-packages/sdl-image/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_REVISION=20
 TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_image/release/SDL_image-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff, sdl, zlib"
+
