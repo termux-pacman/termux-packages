5dd311510683e2214192baf97b2251b9e682602a
Ivan Max <mixython@gmail.com>
2022-10-21 13:39:15 +0300

    build oshu (#1)

--- x11-packages/oshu/build.sh
+++ x11-packages/oshu/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL=https://github.com/fmang/oshu/archive/refs/tags/${TERMUX_PKG_V
 TERMUX_PKG_SHA256=2f576ffd87af58bc246f7e3277ec4193799648b56909a309990ba73af6070ad4
 TERMUX_PKG_DEPENDS="ffmpeg, zstd, xorgproto, libcairo, pango, sdl2, sdl2-image"
 TERMUX_PKG_BUILD_IN_SRC=true
+
