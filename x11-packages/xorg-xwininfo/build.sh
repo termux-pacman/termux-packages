2249b0587782aa1a27743d862a751810ba759d19
Ivan Max <mixython@gmail.com>
2022-10-21 13:52:03 +0300

    build xorg-xwininfo (#1)

--- x11-packages/xorg-xwininfo/build.sh
+++ x11-packages/xorg-xwininfo/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xwininfo-
 TERMUX_PKG_SHA256=7a405441dfc476666c744f5fcd1bc8a75abf8b5b1d85db7b88b370982365080e
 TERMUX_PKG_DEPENDS="libx11, libiconv"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
+
