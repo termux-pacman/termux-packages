3108c1652a4f0c3fcc98b32e7df0532612846f52
Maxython <mixython@gmail.com>
2023-06-04 15:54:29 +0300

    rebuild packages/bastet/build.sh

--- packages/bastet/build.sh
+++ packages/bastet/build.sh
@@ -11,3 +11,4 @@ TERMUX_PKG_BUILD_DEPENDS="boost-headers, boost-static"
 TERMUX_PKG_EXTRA_MAKE_ARGS=" BOOST_PO=$TERMUX_PREFIX/lib/libboost_program_options.a"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_GROUPS="games"
+
