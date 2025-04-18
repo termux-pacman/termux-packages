4a266cb8700b2343bdf695da5f2ece61b172bc4a
Maxython <mixython@gmail.com>
2025-04-18 14:17:12 +0300

    test

---
+++ scripts/build/termux_step_install_pacman_hooks.sh
@@ -0,0 +1,28 @@
+termux_step_install_pacman_hooks() {
+	[ "$TERMUX_PACKAGE_FORMAT" != "pacman" ] && return
+
+	local hooks=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name "*.alpm.hook")
+	if [[ -n "${hooks}" ]]; then
+		mkdir -p ${TERMUX_PREFIX}/share/libalpm/hooks
+		local hook
+		for hook in ${hooks}; do
+			sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+				-e "s|@TERMUX_PREFIX_TARGET@|${TERMUX_PREFIX:1}|g" \
+				"${hook}" > "${TERMUX_PREFIX}/share/libalpm/hooks/$(sed 's|.alpm.hook$|.hook|' <<< ${hook##*/})"
+		done
+	fi
+
+	local scripts=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name "*.alpm.script")
+	if [[ -n "${scripts}" ]]; then
+		mkdir -p ${TERMUX_PREFIX}/share/libalpm/scripts
+		local script
+		local script_alpm
+		for script in ${scripts}; do
+			script_alpm="${TERMUX_PREFIX}/share/libalpm/scripts/$(sed 's|.alpm.script$||' <<< ${script##*/})"
+			sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+				-e "s|@TERMUX_PREFIX_TARGET@|${TERMUX_PREFIX:1}|g" \
+				"${script}" > "${script_alpm}"
+			chmod +x "${script_alpm}"
+		done
+	fi
+}
